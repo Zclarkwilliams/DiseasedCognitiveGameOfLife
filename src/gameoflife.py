f1bcccecde744b740397d56cb5a7f37805223cda
Zachary Clark-Williams <zclarkw112@gmail.com>
2023-01-30 20:45:18 -0800

    remove glider and glider gun functions as they wont be used for our project.

--- src/gameoflife.py
+++ src/gameoflife.py
@@ -29,44 +29,6 @@ def randomGrid(N):
 	"""returns a grid of NxN random values"""
 	return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
 
-def addGlider(i, j, grid):
-
-	"""adds a glider with top left cell at (i, j)"""
-	glider = np.array([[0, 0, 255],
-					[255, 0, 255],
-					[0, 255, 255]])
-	grid[i:i+3, j:j+3] = glider
-
-def addGosperGliderGun(i, j, grid):
-
-	"""adds a Gosper Glider Gun with top left
-	cell at (i, j)"""
-	gun = np.zeros(11*38).reshape(11, 38)
-
-	gun[5][1] = gun[5][2] = 255
-	gun[6][1] = gun[6][2] = 255
-
-	gun[3][13] = gun[3][14] = 255
-	gun[4][12] = gun[4][16] = 255
-	gun[5][11] = gun[5][17] = 255
-	gun[6][11] = gun[6][15] = gun[6][17] = gun[6][18] = 255
-	gun[7][11] = gun[7][17] = 255
-	gun[8][12] = gun[8][16] = 255
-	gun[9][13] = gun[9][14] = 255
-
-	gun[1][25] = 255
-	gun[2][23] = gun[2][25] = 255
-	gun[3][21] = gun[3][22] = 255
-	gun[4][21] = gun[4][22] = 255
-	gun[5][21] = gun[5][22] = 255
-	gun[6][23] = gun[6][25] = 255
-	gun[7][25] = 255
-
-	gun[3][35] = gun[3][36] = 255
-	gun[4][35] = gun[4][36] = 255
-
-	grid[i:i+11, j:j+38] = gun
-
 def update(frameNum, img, grid, N):
 
 	# copy grid since we require 8 neighbors
@@ -108,8 +70,6 @@ def main():
 	parser.add_argument('--grid-size', dest='N', required=False)
 	parser.add_argument('--mov-file', dest='movfile', required=False)
 	parser.add_argument('--interval', dest='interval', required=False)
-	parser.add_argument('--glider', action='store_true', required=False)
-	parser.add_argument('--gosper', action='store_true', required=False)
 	args = parser.parse_args()
 	
 	# set grid size
@@ -124,18 +84,8 @@ def main():
 
 	# declare grid
 	grid = np.array([])
-
-	# check if "glider" demo flag is specified
-	if args.glider:
-		grid = np.zeros(N*N).reshape(N, N)
-		addGlider(1, 1, grid)
-	elif args.gosper:
-		grid = np.zeros(N*N).reshape(N, N)
-		addGosperGliderGun(10, 10, grid)
-
-	else: # populate grid with random on/off -
-			# more off than on
-		grid = randomGrid(N)
+	# populate grid with random on/off - more off than on
+	grid = randomGrid(N)
 
 	# set up animation
 	fig, ax = plt.subplots()
