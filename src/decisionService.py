6d1eac5bbc19a90deaa459e23e17bbce61e8f363
Scott Pickthorn <scott.e.pickthorn@gmail.com>
2023-04-16 17:18:42 -0700

    adding neighbor picking by color

--- src/decisionService.py
+++ src/decisionService.py
@@ -1,7 +1,27 @@
 import random
+import math
+import numpy as np
+from cell import *
 
-EXP1_PROB = .8
+EXP1_PROB = .60
+neighboringCells = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,1],[1,0],[1,-1]]
 
+# base experiment where we pick a random neighbor and a random choice whether to activate the cell or not
 def decide():
-    # to start choose a random neighbor, and a random choice whether to activate it or not
-    return random.choice([[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,1],[1,0],[1,-1]]), random.choice([0,1])
+    return random.choice(neighboringCells), random.choice([0,1])
+# use the color of neighboring cells to pick the closest neighbor based n color
+# give it a random probability of activating the cell (simulating mating)
+def decideV2(currCell, grid):
+    shortestDist = math.inf
+    closestNeighbor = None
+    # for each neighbor calculate the euclidean distance from the 3 dimensional point
+    for neighbor in neighboringCells:
+        if(grid[neighbor[0]][neighbor[1]].life == DEAD):
+            currPoint = np.array(grid[currCell[0]][currCell[1]].state)
+            neighborPoint = np.array(grid[neighbor[0]][neighbor[1]].state)
+            squared_dist = np.sum((currPoint-neighborPoint)**2, axis=0)
+            dist = np.sqrt(squared_dist)
+            if dist < shortestDist:
+                shortestDist = dist
+                closestNeighbor = neighbor
+    return closestNeighbor, random.choice([0,1])
